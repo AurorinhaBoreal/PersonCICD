033f85c300dee80e197e550783507b6082539a9b
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-07-03 17:53:28 -0300

    fix: One Unnecessary Variable

--- src/test/java/com/db/crud/person/unitary/AddressServiceUnitaryTests.java
+++ src/test/java/com/db/crud/person/unitary/AddressServiceUnitaryTests.java
@@ -10,7 +10,6 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
@@ -55,8 +54,6 @@ class AddressServiceUnitaryTests {
 
     PersonRequest personDTOValid = PersonFixture.PersonDTOValidFixture();
     Person personEntityValid = PersonFixture.PersonEntityValidFixture();
-
-    List<Address> addresses = new ArrayList<>();
 
     @Test
     @DisplayName("Happy Testt: Should list addresses")
